aa43abb36d6c85ce8866659801fc3f20ef3311f6
MarcoChanwoo <128105411+MarcoChanwoo@users.noreply.github.com>
2023-08-14 19:16:32 +0900

    [unrated] Title: 배열 만들기 2, Time: 10.64 ms, Memory: 39.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
@@ -0,0 +1,12 @@
+function solution(l, r) {
+    var answer = [];
+    for(let i=l;i<=r;i++){
+        let arr=String(i)
+        if(![...arr].every(x=>x==='5'||x==='0'))continue;
+        answer.push(i)
+        
+    }
+    return answer.length? answer:[-1];
+}
+
+console.log(solution(10,20))
